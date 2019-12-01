2fd1bbd9b81ffc93c0b46efabe45e599c1c03c12
Jithin Das <jithindasad@gmail.com>
2019-12-01 22:47:52 +0530

    Add ceaser_cipher

---
+++ ceaser_shift.rb
@@ -0,0 +1,33 @@
+
+
+def ceaser_cipher(string, number)
+    string_array = string.split(//)
+    alphabets = ("a".."z").to_a
+    upper_alphabets = ("A".."Z").to_a
+    final_array = []
+
+    string_array.each do |char|
+        if char.match(/[a-z]/)
+            char_index = alphabets.index char
+            char_index += number
+            if char_index >= 26
+                char_index -= 26
+            end
+            final_array = final_array << alphabets[char_index]
+        elsif char.match(/[A-Z]/)
+            char_index = upper_alphabets.index char
+            char_index += number
+            if char_index >= 26
+                char_index -= 26
+            end
+            final_array = final_array << upper_alphabets[char_index]
+        else
+            final_array = final_array << char
+        end
+    end
+
+    shifted_string = final_array.join
+    puts shifted_string
+end
+
+ceaser_cipher("What a string!", 5)
