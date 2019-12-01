c98876732f33d1c759ed2de8a085b8ebb3c2e427
Jithin Das <jithindasad@gmail.com>
2019-12-02 00:47:17 +0530

    add comment

--- ceaser_shift.rb
+++ ceaser_shift.rb
@@ -1,4 +1,9 @@
-
+# Implement a caesar cipher that takes in a string and the shift factor
+# and then outputs the modified string:
+#
+#   caesar_cipher("What a string!", 5)
+#   => "Bmfy f xywnsl!"
+#
 
 def ceaser_cipher(string, number)
     string_array = string.split(//)
